dd388f7fcc78b25c270309a48a0253a43276262e
Phil Snyder <subfivemile@gmail.com>
2023-04-28 11:00:51 -0700

    Update lambda README and generate_test_event refactoring

--- src/lambda_function/events/generate_test_event.py
+++ src/lambda_function/events/generate_test_event.py
@@ -1,66 +1,46 @@
 """
 This is a utility script that generates a fake S3 event notification and writes
-it out as JSON. This test event can be used to test the lambda which
+it out as JSON. This test event can be used to test the Lambda which
 triggers the Glue pipeline.
+
+Two types of events can be generated:
+
+    1. An event with a single record (single-records.json).
+    2. An event with multiple records (records.json).
+
+See https://github.com/Sage-Bionetworks/recover/tree/main/src/lambda_function
+for detailed information how to use this event with the Lambda.
 """
 
 import argparse
-import copy
 import json
 import boto3
 
 SINGLE_RECORD_OUTFILE = 'single-record.json'
 MULTI_RECORD_OUTFILE = 'records.json'
 
-s3_event_record_template = {
-  "eventVersion": "2.0",
-  "eventSource": "aws:s3",
-  "awsRegion": "us-east-1",
-  "eventTime": "1970-01-01T00:00:00.000Z",
-  "eventName": "ObjectCreated:Put",
-  "userIdentity": {
-    "principalId": "EXAMPLE"
-  },
-  "requestParameters": {
-    "sourceIPAddress": "127.0.0.1"
-  },
-  "responseElements": {
-    "x-amz-request-id": "EXAMPLE123456789",
-    "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH"
-  },
-  "s3": {
-    "s3SchemaVersion": "1.0",
-    "configurationId": "testConfigRule",
-    "bucket": {
-      "name": "{bucket}",
-      "ownerIdentity": {
-        "principalId": "EXAMPLE"
-      },
-      "arn": "arn:aws:s3:::bucket_arn"
-    },
-    "object": {
-      "key": "{key}",
-      "size": 1024,
-      "eTag": "0123456789abcdef0123456789abcdef",
-      "sequencer": "0A1B2C3D4E5F678901"
-    }
-  }
-}
 
 def read_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-      description="Generate a json file of a mocked SQS event for testing.")
-    parser.add_argument("--input-bucket",
-      default="recover-dev-input-data",
-      help="Optional. S3 bucket name containing input data. Default is 'recover-dev-input-data'.")
-    parser.add_argument("--input-key",
-      default="main/2023-01-12T22--02--17Z_77fefff8-b0e2-4c1b-b0c5-405554c92368",
-      help=("Optional. Name of the Synapse dataset containing test data. "
-            "Default is `main/2023-01-12T22--02--17Z_77fefff8-b0e2-4c1b-b0c5-405554c92368` "))
-    parser.add_argument("--input-key-prefix",
-      help=("Optional. Takes precedence over `--input-key`. If you want to generate a "
-            "single event containing all data under a specific S3 key prefix, "
-            "specify that here."))
+            description="Generate a JSON file of a mocked S3 event for testing.",
+            formatter_class = argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+            "--input-bucket",
+            default="recover-dev-input-data",
+            help="S3 bucket name containing input data"
+    )
+    parser.add_argument(
+            "--input-key",
+            default="main/2023-01-12T22--02--17Z_77fefff8-b0e2-4c1b-b0c5-405554c92368",
+            help="Name of the Synapse dataset containing test data."
+    )
+    parser.add_argument(
+            "--input-key-prefix",
+            help=("Takes precedence over `--input-key`. If you want "
+                  "to generate a single event containing all data under a specific "
+                  "S3 key prefix, specify that here.")
+    )
     args = parser.parse_args()
     return args
 
@@ -109,10 +89,43 @@ def create_s3_event_record(bucket: str, key: str) -> dict:
         dict: A dictionary formatted as a "Record" object would be
             in an S3 event notification
     """
-    s3_record = copy.deepcopy(s3_event_record_template)
-    s3_record["s3"]["bucket"]["name"] = bucket
-    s3_record["s3"]["object"]["key"] = key
-    return s3_record
+    s3_event_record = {
+      "eventVersion": "2.0",
+      "eventSource": "aws:s3",
+      "awsRegion": "us-east-1",
+      "eventTime": "1970-01-01T00:00:00.000Z",
+      "eventName": "ObjectCreated:Put",
+      "userIdentity": {
+        "principalId": "EXAMPLE"
+      },
+      "requestParameters": {
+        "sourceIPAddress": "127.0.0.1"
+      },
+      "responseElements": {
+        "x-amz-request-id": "EXAMPLE123456789",
+        "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH"
+      },
+      "s3": {
+        "s3SchemaVersion": "1.0",
+        "configurationId": "testConfigRule",
+        "bucket": {
+          "name": "{bucket}",
+          "ownerIdentity": {
+            "principalId": "EXAMPLE"
+          },
+          "arn": "arn:aws:s3:::bucket_arn"
+        },
+        "object": {
+          "key": "{key}",
+          "size": 1024,
+          "eTag": "0123456789abcdef0123456789abcdef",
+          "sequencer": "0A1B2C3D4E5F678901"
+        }
+      }
+    }
+    s3_event_record["s3"]["bucket"]["name"] = bucket
+    s3_event_record["s3"]["object"]["key"] = key
+    return s3_event_record
 
 def main() -> None:
     args = read_args()
