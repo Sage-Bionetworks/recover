1952d2ad365ccc10c77847d5202194b7f6553425
thomasyu888 <thomas.yu@sagebase.org>
2023-05-06 15:34:45 -0700

    Add black

--- src/glue/jobs/s3_to_json.py
+++ src/glue/jobs/s3_to_json.py
@@ -16,12 +16,14 @@ import zipfile
 import boto3
 from awsglue.utils import getResolvedOptions
 
+
 def write_file_to_json_dataset(
-        z: zipfile.ZipFile,
-        json_path: str,
-        dataset_identifier: str,
-        metadata: dict,
-        workflow_run_properties: dict) -> str:
+    z: zipfile.ZipFile,
+    json_path: str,
+    dataset_identifier: str,
+    metadata: dict,
+    workflow_run_properties: dict,
+) -> str:
     """
     Write a JSON from a zipfile to a JSON dataset.
 
@@ -62,10 +64,9 @@ def write_file_to_json_dataset(
                 j["Value"] = json.loads(j["Value"])
             if dataset_identifier == "EnrolledParticipants":
                 for field_name in ["Symptoms", "Treatments"]:
-                    if (
-                            field_name in j["CustomFields"]
-                            and isinstance(j["CustomFields"][field_name], str)
-                       ):
+                    if field_name in j["CustomFields"] and isinstance(
+                        j["CustomFields"][field_name], str
+                    ):
                         if len(j["CustomFields"][field_name]) > 0:
                             # This JSON string was written in a couple different ways
                             # in the testing data: "[{\\\"id\\\": ..." and "[{\"id\": ..."
@@ -74,13 +75,15 @@ def write_file_to_json_dataset(
                             # an object rather than a string, so...).
                             try:
                                 j["CustomFields"][field_name] = json.loads(
-                                        j["CustomFields"][field_name]
+                                    j["CustomFields"][field_name]
                                 )
                             except json.JSONDecodeError as e:
                                 # If it's not propertly formatted JSON, then we
                                 # can't read it, and instead store an empty list
-                                logger.warning(f"Problem CustomFields.{field_name}: "
-                                               f"{j['CustomFields'][field_name]}")
+                                logger.warning(
+                                    f"Problem CustomFields.{field_name}: "
+                                    f"{j['CustomFields'][field_name]}"
+                                )
                                 logger.warning(str(e))
                                 j["CustomFields"][field_name] = []
                         else:
@@ -88,40 +91,42 @@ def write_file_to_json_dataset(
             data.append(j)
     if dataset_identifier in data_types_with_subtype:
         output_fname = "{}_{}_{}-{}.ndjson".format(
-                metadata["type"],
-                metadata["subtype"],
-                metadata["start_date"].strftime("%Y%m%d"),
-                metadata["end_date"].strftime("%Y%m%d")
+            metadata["type"],
+            metadata["subtype"],
+            metadata["start_date"].strftime("%Y%m%d"),
+            metadata["end_date"].strftime("%Y%m%d"),
         )
     elif metadata["start_date"] is None:
         output_fname = "{}_{}.ndjson".format(
-                metadata["type"],
-                metadata["end_date"].strftime("%Y%m%d")
-    )
+            metadata["type"], metadata["end_date"].strftime("%Y%m%d")
+        )
     else:
         output_fname = "{}_{}-{}.ndjson".format(
-                metadata["type"],
-                metadata["start_date"].strftime("%Y%m%d"),
-                metadata["end_date"].strftime("%Y%m%d")
+            metadata["type"],
+            metadata["start_date"].strftime("%Y%m%d"),
+            metadata["end_date"].strftime("%Y%m%d"),
         )
     output_path = os.path.join(dataset_identifier, output_fname)
     s3_output_key = os.path.join(
         workflow_run_properties["namespace"],
         workflow_run_properties["json_prefix"],
         f"dataset={dataset_identifier}",
-        output_fname)
+        output_fname,
+    )
     logger.debug("Output Key: %s", s3_output_key)
     with open(output_path, "w") as f_out:
         for record in data:
             f_out.write("{}\n".format(json.dumps(record)))
     with open(output_path, "rb") as f_in:
         response = s3_client.put_object(
-                Body = f_in,
-                Bucket = workflow_run_properties["json_bucket"],
-                Key = s3_output_key,
-                Metadata = s3_metadata)
+            Body=f_in,
+            Bucket=workflow_run_properties["json_bucket"],
+            Key=s3_output_key,
+            Metadata=s3_metadata,
+        )
         logger.debug("S3 Put object response: %s", json.dumps(response))
     return output_path
+
 
 def get_metadata(basename: str) -> dict:
     """
@@ -148,14 +153,13 @@ def get_metadata(basename: str) -> dict:
     metadata["type"] = basename_components[0]
     if "-" in basename_components[-1]:
         start_date, end_date = basename_components[-1].split("-")
-        metadata["start_date"] = \
-                datetime.datetime.strptime(start_date, "%Y%m%d")
-        metadata["end_date"] = \
-                datetime.datetime.strptime(end_date, "%Y%m%d")
+        metadata["start_date"] = datetime.datetime.strptime(start_date, "%Y%m%d")
+        metadata["end_date"] = datetime.datetime.strptime(end_date, "%Y%m%d")
     else:
         metadata["start_date"] = None
-        metadata["end_date"] = \
-                datetime.datetime.strptime(basename_components[-1], "%Y%m%d")
+        metadata["end_date"] = datetime.datetime.strptime(
+            basename_components[-1], "%Y%m%d"
+        )
     if metadata["type"] in data_types_with_subtype:
         metadata["subtype"] = basename_components[1]
     if (
@@ -172,9 +176,8 @@ def get_metadata(basename: str) -> dict:
     logger.debug("metadata = %s", metadata)
     return metadata
 
-def process_record(
-        s3_obj: dict,
-        workflow_run_properties: dict) -> None:
+
+def process_record(s3_obj: dict, workflow_run_properties: dict) -> None:
     """
     Write the contents of a .zip archive stored on S3 to their respective
     JSON dataset.
@@ -192,24 +195,26 @@ def process_record(
     logger = logging.getLogger(__name__)
     with zipfile.ZipFile(io.BytesIO(s3_obj["Body"])) as z:
         non_empty_contents = [
-                f.filename for f in z.filelist
-                if "/" not in f.filename and
-                "Manifest" not in f.filename and
-                f.file_size > 0
+            f.filename
+            for f in z.filelist
+            if "/" not in f.filename
+            and "Manifest" not in f.filename
+            and f.file_size > 0
         ]
         logger.debug("contents: %s", z.namelist())
         logger.debug("non-empty contents: %s", non_empty_contents)
         for json_path in non_empty_contents:
             metadata = get_metadata(os.path.basename(json_path))
             dataset_identifier = metadata["type"]
-            logger.info("Writing %s to dataset %s",
-                        json_path, dataset_identifier)
+            logger.info("Writing %s to dataset %s", json_path, dataset_identifier)
             write_file_to_json_dataset(
-                    z=z,
-                    json_path=json_path,
-                    dataset_identifier=dataset_identifier,
-                    metadata=metadata,
-                    workflow_run_properties=workflow_run_properties)
+                z=z,
+                json_path=json_path,
+                dataset_identifier=dataset_identifier,
+                metadata=metadata,
+                workflow_run_properties=workflow_run_properties,
+            )
+
 
 def main() -> None:
     # Configure logger
@@ -222,15 +227,10 @@ def main() -> None:
     s3_client = boto3.client("s3")
 
     # Get job and workflow arguments
-    args = getResolvedOptions(
-            sys.argv,
-            ["WORKFLOW_NAME",
-             "WORKFLOW_RUN_ID"
-            ]
-    )
+    args = getResolvedOptions(sys.argv, ["WORKFLOW_NAME", "WORKFLOW_RUN_ID"])
     workflow_run_properties = glue_client.get_workflow_run_properties(
-            Name=args["WORKFLOW_NAME"],
-            RunId=args["WORKFLOW_RUN_ID"])["RunProperties"]
+        Name=args["WORKFLOW_NAME"], RunId=args["WORKFLOW_RUN_ID"]
+    )["RunProperties"]
     logger.debug("getResolvedOptions: %s", json.dumps(args))
     logger.debug("get_workflow_run_properties: %s", json.dumps(workflow_run_properties))
 
@@ -238,17 +238,17 @@ def main() -> None:
     logger.info("Loading messages")
     messages = json.loads(workflow_run_properties["messages"])
     for message in messages:
-        logger.info("Retrieving S3 object for Bucket %s and Key %s",
-                    message["source_bucket"], message["source_key"])
+        logger.info(
+            "Retrieving S3 object for Bucket %s and Key %s",
+            message["source_bucket"],
+            message["source_key"],
+        )
         s3_obj = s3_client.get_object(
-                Bucket = message["source_bucket"],
-                Key = message["source_key"]
+            Bucket=message["source_bucket"], Key=message["source_key"]
         )
         s3_obj["Body"] = s3_obj["Body"].read()
-        process_record(
-                s3_obj=s3_obj,
-                workflow_run_properties=workflow_run_properties
-        )
+        process_record(s3_obj=s3_obj, workflow_run_properties=workflow_run_properties)
+
 
 if __name__ == "__main__":
     main()
