4dbff1721fd0d77cb1560a8948817ada15b94a33
rxu17 <26471741+rxu17@users.noreply.github.com>
2023-05-16 10:58:53 -0700

    [ETL-397] Create s3-event-config lambda and dependencies to add s3 notification configuration (#50)
    * Incorporate new s3-event-config lambda code, stacks and role into ETL workflow
    * Update lambda structure to have separate folders for s3 to glue and s3 event config lambdas
    
    ---------
    
    Co-authored-by: Rixing Xu <rxu@w197.local>

---
+++ src/lambda_function/s3_event_config/README.md
@@ -0,0 +1,42 @@
+# s3_event_config lambda
+
+The s3_event_config lambda is triggered by a github action during deployment or manually through the AWS console.
+
+It will then put a S3 event notification configuration into
+the input data bucket which allows the input data bucket to
+trigger the S3 to JSON lambda with S3 new object notifications whenever new objects are added
+to it and eventually lead to the start of the S3-to-JSON workflow.
+
+## Event format
+
+The events that will trigger the s3-event-config-lambda
+should be in the form of something like:
+
+```
+{
+    "RequestType": "Create"
+}
+```
+
+Where the allowed RequestType values are:
+- "Create"
+- "Update"
+- "Delete"
+
+## Launching Lambda stack in AWS
+
+There are two main stacks involved in the s3_event_config lambda. They are the
+`s3_event_config lambda role` stack and the `s3_event_config lambda` stack.
+
+Note that they depend on the `s3 to json` lambda stacks.
+
+### Sceptre
+
+#### Launching in development
+
+Run the following command to create the lambda stack in your AWS account. Note this will
+also create the lambda event config IAM role stack as well as well as any other dependencies of this stack:
+
+```shell script
+sceptre --var namespace='test-namespace' launch develop/namespaced/s3-event-config-lambda.yaml
+```
---
+++ src/lambda_function/s3_event_config/app.py
@@ -0,0 +1,87 @@
+"""
+This Lambda app responds to an external trigger (usually github action or aws console) and
+puts a s3 event notification configuration for the S3 to Glue lambda in the
+input data S3 bucket set by the environment variable `S3_SOURCE_BUCKET_NAME`.
+
+This Lambda app also has the option of deleting the notification configuration
+from an S3 bucket
+"""
+import os
+import json
+import logging
+
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+REQUEST_TYPE_VALS = ["Delete", "Create", "Update"]
+
+
+def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+    logger.info(f"Received event: {json.dumps(event, indent=2)}")
+    if event["RequestType"] == "Delete":
+        logger.info(f'Request Type:{event["RequestType"]}')
+        delete_notification(s3, bucket=os.environ["S3_SOURCE_BUCKET_NAME"])
+        logger.info("Sending response to custom resource after Delete")
+    elif event["RequestType"] in ["Update", "Create"]:
+        logger.info(f'Request Type: {event["RequestType"]}')
+        add_notification(
+            s3,
+            lambda_arn=os.environ["S3_TO_GLUE_FUNCTION_ARN"],
+            bucket=os.environ["S3_SOURCE_BUCKET_NAME"],
+            bucket_key_prefix=os.environ["BUCKET_KEY_PREFIX"],
+        )
+        logger.info("Sending response to custom resource")
+    else:
+        err_msg = f"The 'RequestType' key should have one of the following values: {REQUEST_TYPE_VALS}"
+        raise KeyError(err_msg)
+
+
+def add_notification(
+    s3_client: boto3.client,
+    lambda_arn: str,
+    bucket: str,
+    bucket_key_prefix: str,
+):
+    """Adds the S3 notification configuration to an existing bucket
+
+    Args:
+        s3_client (boto3.client) : s3 client to use for s3 event config
+        lambda_arn (str): Arn of the lambda s3 event config function
+        bucket (str): bucket name of the s3 bucket to add the config to
+        bucket_key_prefix (str): bucket key prefix for where to look for s3 object notifications
+    """
+    s3_client.put_bucket_notification_configuration(
+        Bucket=bucket,
+        NotificationConfiguration={
+            "LambdaFunctionConfigurations": [
+                {
+                    "LambdaFunctionArn": lambda_arn,
+                    "Events": ["s3:ObjectCreated:*"],
+                    "Filter": {
+                        "Key": {
+                            "FilterRules": [
+                                {"Name": "prefix", "Value": bucket_key_prefix}
+                            ]
+                        }
+                    },
+                }
+            ]
+        },
+    )
+    logger.info("Put request completed....")
+
+
+def delete_notification(s3_client: boto3.client, bucket: str):
+    """Deletes the S3 notification configuration from an existing bucket
+
+    Args:
+        s3_client (boto3.client) : s3 client to use for s3 event config
+        bucket (str): bucket name of the s3 bucket to delete the config in
+    """
+    s3_client.put_bucket_notification_configuration(
+        Bucket=bucket, NotificationConfiguration={}
+    )
+    logger.info("Delete request completed....")
