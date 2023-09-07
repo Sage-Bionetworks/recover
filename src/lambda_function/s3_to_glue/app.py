12bb229614037a3e6e8be5cbcf10925c8a4c7caa
Rixing Xu <rxu@w197.local>
2023-09-07 12:18:22 -0700

    update s3 to json lambda role to be namespaced, add scheduled trigger, update lambda code to read in sqs queue message and filter it

--- src/lambda_function/s3_to_glue/app.py
+++ src/lambda_function/s3_to_glue/app.py
@@ -12,14 +12,56 @@ import boto3
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def submit_s3_to_json_workflow(messages: list[dict[str,str]], workflow_name):
+
+def filter_object_info(object_info: dict) -> dict:
+    """Filter out object info records that should not be processed. Returns None
+
+       - Records containing owner.txt
+       - Records that don't contain a specific object key like <path_to_file>/<file_name>
+       - Records that are missing a source_key
+       - Records that are missing a bucket_name
+
+    Args:
+        object_info (dict): Object information from source S3 bucket
+
+    Returns:
+        dict: returns back the object info dict if it passes the filter criteria
+            otherwise returns None
+    """
+    if not object_info["source_key"]:
+        logger.info(
+            "This object_info record doesn't contain a source key "
+            f"and can't be processed.\nMessage: {object_info}",
+        )
+        return None
+    elif not object_info["source_bucket"]:
+        logger.info(
+            "This object_info record doesn't contain a source bucket "
+            f"and can't be processed.\nMessage: {object_info}",
+        )
+        return None
+    elif "owner.txt" in object_info["source_key"]:
+        logger.info(
+            f"This object_info record is an owner.txt and can't be processed.\nMessage: {object_info}"
+        )
+        return None
+    elif object_info["source_key"].endswith("/"):
+        logger.info(
+            f"This object_info record is a directory and can't be processed.\nMessage: {object_info}"
+        )
+        return None
+    else:
+        return object_info
+
+
+def submit_s3_to_json_workflow(objects_info: list[dict[str, str]], workflow_name):
     """
     Submits list of dicts with keys `source_bucket` and `source_key`
     to the S3 to JSON Glue workflow.
 
     Args:
         workflow_name (str): name of the glue workflow to submit to
-        messages (list): objects from source S3 bucket to submit
+        objects_info (list): objects from source S3 bucket to submit
         to Glue workflow.
 
     Returns:
@@ -31,38 +73,86 @@ def submit_s3_to_json_workflow(messages: list[dict[str,str]], workflow_name):
     glue_client.put_workflow_run_properties(
         Name=workflow_name,
         RunId=workflow_run["RunId"],
-        RunProperties={"messages": json.dumps(messages)},
+        RunProperties={"messages": json.dumps(objects_info)},
     )
 
 
-def lambda_handler(event, context):
-    """
-    The Lambda entrypoint
-
-    Given a list of files in s3 bucket to process, submit the keys and bucket
-    of the new files to a S3 to JSON Glue workflow.
+def lambda_handler(event, context) -> None:
+    """This main lambda function will be triggered by a
+        cloudwatch scheduler
 
     Args:
-        event (dict): A fileview object
-        context (obj) : An object that provides methods and properties that
-        provides info about the invocation, function, and execution environment
+        event (json): CloudWatch scheduled trigger event
+        context: NA
 
-    Returns:
-        (None) Submits new file records to a Glue workflow.
+    Raises:
+        Exception: When polling SQS queue on a loop has reached the maximum iteration
     """
-    messages = []
-    for record in event["Records"]:
-        bucket_name = record["s3"]["bucket"]["name"]
-        object_key = record["s3"]["object"]["key"]
-        message = {"source_bucket": bucket_name, "source_key": object_key}
-        if "owner.txt" not in object_key:
-            messages.append(message)
-    if len(messages) > 0:
-        logger.info(
-                "Submitting the following files to "
-                f"{os.environ['PRIMARY_WORKFLOW_NAME']}: {json.dumps(messages)}"
+    # Initialize SQS client
+    sqs = boto3.client("sqs")
+
+    # Get the URL of the SQS queue
+    queue_name = os.environ["SQS_QUEUE_NAME"]
+    response = sqs.get_queue_url(QueueName=queue_name)
+    queue_url = response["QueueUrl"]
+
+    has_messages = True
+
+    # maximum iterations for any loop in order to timeout a loop
+    max_iterations = 1000
+    iteration = 0
+    while has_messages:
+        if iteration > max_iterations:
+            raise Exception(
+                f"Polling SQS queue reached maximum iterations of {max_iterations}"
+            )
+        # Receive up to 10 messages at a time
+        # Note that this visibility timeout var overrides the SQS queue's default and
+        # sets a different visibility timeout for these specific messages
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            VisibilityTimeout=int(os.environ["VISIBILITY_TIMEOUT"]),
+            MaxNumberOfMessages=int(os.environ["MAX_NUMBER_OF_MESSAGES_TO_RECEIVE"]),
+            WaitTimeSeconds=int(os.environ["WAIT_TIME_FOR_MESSAGES"]),
         )
-        submit_s3_to_json_workflow(
-                messages = messages,
-                workflow_name = os.environ["PRIMARY_WORKFLOW_NAME"]
-        )
+        # Check if messages were received
+        if "Messages" in response:
+            messages = response["Messages"]
+            for message in messages:
+                msg_body = json.loads(message["Body"])
+                s3_objects_info = []
+                # check that records are present per message
+                if "Records" in msg_body:
+                    for record in msg_body["Records"]:
+                        bucket_name = record["s3"]["bucket"]["name"]
+                        object_key = record["s3"]["object"]["key"]
+                        object_info = {
+                            "source_bucket": bucket_name,
+                            "source_key": object_key,
+                        }
+                        if filter_object_info(object_info) is not None:
+                            s3_objects_info.append(object_info)
+                    if len(s3_objects_info) > 0:
+                        logger.info(
+                            "Submitting the following files to "
+                            f"{os.environ['PRIMARY_WORKFLOW_NAME']}: {json.dumps(s3_objects_info)}"
+                        )
+                        submit_s3_to_json_workflow(
+                            objects_info=s3_objects_info,
+                            workflow_name=os.environ["PRIMARY_WORKFLOW_NAME"],
+                        )
+                else:
+                    logger.info(
+                        "No 'Records' key exists in event to be parsed"
+                        f"\nEvent: {event}"
+                    )
+                # Delete the messages from the queue
+                receipt_handle = message["ReceiptHandle"]
+                sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
+            has_messages = False
+            iteration += 1
+        else:
+            has_messages = False
+            return {"statusCode": 0, "body": "No messages exist"}
+
+    return {"statusCode": 200, "body": "All messages retrieved and processed."}
--- tests/test_s3_to_glue_lambda.py
+++ tests/test_s3_to_glue_lambda.py
@@ -1,5 +1,12 @@
 import pytest
+from unittest import mock
+
+import json
+import boto3
+from moto import mock_sqs
+
 from src.lambda_function.s3_to_glue import app
+
 
 class MockGlueClient:
     def start_workflow_run(*args, **kwargs):
@@ -8,63 +15,297 @@ class MockGlueClient:
     def put_workflow_run_properties(*args, **kwargs):
         return {}
 
+
 class TestS3ToGlueLambda:
+    @pytest.fixture
+    def test_sqs_queue_name(self):
+        yield "test_sqs"
+
+    @pytest.fixture
+    def empty_queue(self):
+        yield {}
+
+    @pytest.fixture
+    def no_records_s3_event(self):
+        sqs_msg = {
+            "Messages": [
+                {
+                    "MessageId": "string",
+                    "ReceiptHandle": "string",
+                    "MD5OfBody": "string",
+                    "Body": "string",
+                    "Attributes": {
+                        "string": "string",
+                    },
+                    "MD5OfMessageAttributes": "string",
+                    "MessageAttributes": {
+                        "string": {
+                            "DataType": "string",
+                            "StringValue": "string",
+                            "BinaryValue": "string",
+                        },
+                    },
+                }
+            ]
+        }
+        s3_event = '{"Records": []}'
+        sqs_msg["Messages"][0]["Body"] = s3_event
+        yield sqs_msg
 
     @pytest.fixture
     def test_s3_event(self):
         s3_event = {
-          "Records": [
-            {
-              "eventVersion": "2.0",
-              "eventSource": "aws:s3",
-              "awsRegion": "us-east-1",
-              "eventTime": "1970-01-01T00:00:00.000Z",
-              "eventName": "ObjectCreated:Put",
-              "userIdentity": {
-                "principalId": "EXAMPLE"
-              },
-              "requestParameters": {
-                "sourceIPAddress": "127.0.0.1"
-              },
-              "responseElements": {
-                "x-amz-request-id": "EXAMPLE123456789",
-                "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH"
-              },
-              "s3": {
-                "s3SchemaVersion": "1.0",
-                "configurationId": "testConfigRule",
-                "bucket": {
-                  "name": "recover-dev-input-data",
-                  "ownerIdentity": {
-                    "principalId": "EXAMPLE"
-                  },
-                  "arn": "arn:aws:s3:::bucket_arn"
-                },
-                "object": {
-                  "key": "main/2023-01-12T22--02--17Z_77fefff8-b0e2-4c1b-b0c5-405554c92368",
-                  "size": 1024,
-                  "eTag": "0123456789abcdef0123456789abcdef",
-                  "sequencer": "0A1B2C3D4E5F678901"
+            "Records": [
+                {
+                    "eventVersion": "2.0",
+                    "eventSource": "aws:s3",
+                    "awsRegion": "us-east-1",
+                    "eventTime": "1970-01-01T00:00:00.000Z",
+                    "eventName": "ObjectCreated:Put",
+                    "userIdentity": {"principalId": "EXAMPLE"},
+                    "requestParameters": {"sourceIPAddress": "127.0.0.1"},
+                    "responseElements": {
+                        "x-amz-request-id": "EXAMPLE123456789",
+                        "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH",
+                    },
+                    "s3": {
+                        "s3SchemaVersion": "1.0",
+                        "configurationId": "testConfigRule",
+                        "bucket": {
+                            "name": "recover-dev-input-data",
+                            "ownerIdentity": {"principalId": "EXAMPLE"},
+                            "arn": "arn:aws:s3:::bucket_arn",
+                        },
+                        "object": {
+                            "key": "main/2023-01-12T22--02--17Z_77fefff8-b0e2-4c1b-b0c5-405554c92368",
+                            "size": 1024,
+                            "eTag": "0123456789abcdef0123456789abcdef",
+                            "sequencer": "0A1B2C3D4E5F678901",
+                        },
+                    },
                 }
-              }
-            }
-          ]
+            ]
         }
         return s3_event
 
     @pytest.fixture
-    def test_messages(self):
-        messages = [
+    def test_message(self, test_s3_event):
+        sqs_msg = {
+            "Messages": [
                 {
-                    "source_bucket": "recover-dev-input-data",
-                    "source_object": "main/2023-01-12T22--02--17Z_77fefff8-b0e2-4c1b-b0c5-405554c92368"
+                    "MessageId": "string",
+                    "ReceiptHandle": "string",
+                    "MD5OfBody": "string",
+                    "Body": "string",
+                    "Attributes": {
+                        "string": "string",
+                    },
+                    "MD5OfMessageAttributes": "string",
+                    "MessageAttributes": {
+                        "string": {
+                            "DataType": "string",
+                            "StringValue": "string",
+                            "BinaryValue": "string",
+                        },
+                    },
                 }
-        ]
-        return messages
+            ]
+        }
+        sqs_msg["Messages"][0]["Body"] = json.dumps(test_s3_event)
+        yield sqs_msg
 
-    def test_submit_s3_to_json_workflow(self, test_messages, monkeypatch):
+    @pytest.fixture
+    def test_object_info(self):
+        object_info = [
+            {
+                "source_bucket": "recover-dev-input-data",
+                "source_key": "main/2023-01-12T22--02--17Z_77fefff8-b0e2-4c1b-b0c5-405554c92368",
+            }
+        ]
+        return object_info
+
+    @pytest.fixture
+    def test_set_env_var(self, monkeypatch, test_sqs_queue_name):
+        monkeypatch.setenv("SQS_QUEUE_NAME", test_sqs_queue_name)
+        monkeypatch.setenv("VISIBILITY_TIMEOUT", 10)
+        monkeypatch.setenv("MAX_NUMBER_OF_MESSAGES_TO_RECEIVE", 10)
+        monkeypatch.setenv("WAIT_TIME_FOR_MESSAGES", 5)
+        monkeypatch.setenv("PRIMARY_WORKFLOW_NAME", "test_workflow")
+
+    def test_submit_s3_to_json_workflow(self, test_object_info, monkeypatch):
         monkeypatch.setattr("boto3.client", lambda x: MockGlueClient())
         app.submit_s3_to_json_workflow(
-                messages=test_messages,
-                workflow_name="example-workflow"
+            objects_info=test_object_info, workflow_name="example-workflow"
         )
+
+    @mock_sqs
+    def test_that_lambda_handler_does_not_call_submit_s3_to_json_workflow_if_empty_event(
+        self, empty_queue, test_sqs_queue_name, test_set_env_var
+    ):
+        with mock_sqs():
+            client = boto3.client("sqs")
+            client.create_queue(QueueName=test_sqs_queue_name)
+            queue_url = client.get_queue_url(QueueName=test_sqs_queue_name)
+
+            with mock.patch.object(boto3, "client") as patch_client, mock.patch.object(
+                app, "submit_s3_to_json_workflow"
+            ) as patch_submit:
+                patch_client.return_value.get_queue_url.return_value = queue_url
+                patch_client.return_value.receive_message.return_value = empty_queue
+                result = app.lambda_handler(event=None, context=None)
+                assert result == {"statusCode": 0, "body": "No messages exist"}
+
+                patch_client.return_value.receive_message.assert_called_once_with(
+                    QueueUrl=queue_url["QueueUrl"],
+                    VisibilityTimeout=10,
+                    MaxNumberOfMessages=10,
+                    WaitTimeSeconds=5,
+                )
+                patch_submit.assert_not_called()
+                patch_client.return_value.assert_not_called()
+
+    @mock_sqs
+    def test_that_lambda_handler_does_not_call_submit_s3_to_json_workflow_if_no_records(
+        self, no_records_s3_event, test_sqs_queue_name, test_set_env_var
+    ):
+        with mock_sqs():
+            client = boto3.client("sqs")
+            client.create_queue(QueueName=test_sqs_queue_name)
+            queue_url = client.get_queue_url(QueueName=test_sqs_queue_name)
+
+            with mock.patch.object(boto3, "client") as patch_client, mock.patch.object(
+                app, "submit_s3_to_json_workflow"
+            ) as patch_submit:
+                patch_client.return_value.get_queue_url.return_value = queue_url
+                patch_client.return_value.receive_message.return_value = (
+                    no_records_s3_event
+                )
+                result = app.lambda_handler(event=None, context=None)
+                assert result == {
+                    "statusCode": 200,
+                    "body": "All messages retrieved and processed.",
+                }
+                patch_client.return_value.receive_message.assert_called_once_with(
+                    QueueUrl=queue_url["QueueUrl"],
+                    VisibilityTimeout=10,
+                    MaxNumberOfMessages=10,
+                    WaitTimeSeconds=5,
+                )
+                patch_submit.assert_not_called()
+                patch_client.return_value.delete_message.assert_called_once_with(
+                    QueueUrl=queue_url["QueueUrl"],
+                    ReceiptHandle=no_records_s3_event["Messages"][0]["ReceiptHandle"],
+                )
+
+    @mock_sqs
+    def test_that_lambda_handler_calls_submit_s3_to_json_workflow_if_queue_has_message(
+        self, test_message, test_sqs_queue_name, test_object_info, test_set_env_var
+    ):
+        with mock_sqs():
+            client = boto3.client("sqs")
+            client.create_queue(QueueName=test_sqs_queue_name)
+            queue_url = client.get_queue_url(QueueName=test_sqs_queue_name)
+
+            with mock.patch.object(boto3, "client") as patch_client, mock.patch.object(
+                app, "submit_s3_to_json_workflow"
+            ) as patch_submit:
+                patch_client.return_value.get_queue_url.return_value = queue_url
+                patch_client.return_value.receive_message.return_value = test_message
+                result = app.lambda_handler(event=None, context=None)
+                assert result == {
+                    "statusCode": 200,
+                    "body": "All messages retrieved and processed.",
+                }
+                patch_client.return_value.receive_message.assert_called_once_with(
+                    QueueUrl=queue_url["QueueUrl"],
+                    VisibilityTimeout=10,
+                    MaxNumberOfMessages=10,
+                    WaitTimeSeconds=5,
+                )
+                patch_submit.assert_called_once_with(
+                    objects_info=test_object_info,
+                    workflow_name="test_workflow",
+                )
+                patch_client.return_value.delete_message.assert_called_once_with(
+                    QueueUrl=queue_url["QueueUrl"],
+                    ReceiptHandle=test_message["Messages"][0]["ReceiptHandle"],
+                )
+
+    def test_that_lambda_handler_does_expected_message_processing_and_handling(
+        self, test_sqs_queue_name, test_s3_event, test_set_env_var
+    ):
+        with mock_sqs():
+            client = boto3.client("sqs")
+            client.create_queue(QueueName=test_sqs_queue_name)
+            queue_url = client.get_queue_url(QueueName=test_sqs_queue_name)
+            test_s3_event_str = json.dumps(test_s3_event)
+
+            # Send a sample message to the SQS queue
+            client.send_message(
+                QueueUrl=queue_url["QueueUrl"], MessageBody=test_s3_event_str
+            )
+            with mock.patch.object(app, "submit_s3_to_json_workflow"):
+                app.lambda_handler(event=None, context=None)
+
+                response = client.receive_message(
+                    QueueUrl=queue_url["QueueUrl"],
+                    VisibilityTimeout=10,
+                    MaxNumberOfMessages=10,
+                    WaitTimeSeconds=10,
+                )
+                assert "Messages" not in response
+
+    @pytest.mark.parametrize(
+        "test_object_info,expected",
+        [
+            (
+                {
+                    "source_bucket": "recover-dev-input-data",
+                    "source_key": "main/2023-01-12T22--02--17Z_77fefff8-b0e2-4c1b-b0c5-405554c92368",
+                },
+                {
+                    "source_bucket": "recover-dev-input-data",
+                    "source_key": "main/2023-01-12T22--02--17Z_77fefff8-b0e2-4c1b-b0c5-405554c92368",
+                },
+            ),
+            (
+                {
+                    "source_bucket": "recover-dev-input-data",
+                    "source_key": "main/v1/owner.txt",
+                },
+                None,
+            ),
+            (
+                {
+                    "source_bucket": "recover-dev-input-data",
+                    "source_key": "main/adults_v2/",
+                },
+                None,
+            ),
+            (
+                {
+                    "source_bucket": "recover-dev-input-data",
+                    "source_key": None,
+                },
+                None,
+            ),
+            (
+                {
+                    "source_bucket": None,
+                    "source_key": "main/2023-01-12T22--02--17Z_77fefff8-b0e2-4c1b-b0c5-405554c92368",
+                },
+                None,
+            ),
+        ],
+        ids=[
+            "correct_msg_format",
+            "owner_txt",
+            "directory",
+            "missing_source_key",
+            "missing_source_bucket",
+        ],
+    )
+    def test_that_filter_object_info_returns_expected_result(
+        self, test_object_info, expected
+    ):
+        assert app.filter_object_info(test_object_info) == expected
