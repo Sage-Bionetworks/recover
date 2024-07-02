2d6782cc452c675f016d9471d49c3ef5a36e89f0
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-02 12:25:30 -0700

    Require cleanup before integration test run

--- src/scripts/manage_artifacts/clean_staging.py
+++ src/scripts/manage_artifacts/clean_staging.py
@@ -13,7 +13,7 @@ def read_args() -> argparse.Namespace:
 
 
 def delete_objects(bucket_prefix: str, bucket: str) -> None:
-    """Handle cleaning up bucket in S3. This will allow the owner.txt file 
+    """Handle cleaning up bucket in S3. This will allow the owner.txt file
     to be kept.
 
     Arguments:
@@ -21,23 +21,19 @@ def delete_objects(bucket_prefix: str, bucket: str) -> None:
         bucket: The name of the bucket to clean
     """
     print(f"Cleaning bucket: {bucket} with prefix: {bucket_prefix}")
-    # Initialize the S3 client
-    s3_client = boto3.client('s3')
 
-    # List all objects in the specified path
+    s3_client = boto3.client('s3')
     response = s3_client.list_objects_v2(Bucket=bucket, Prefix=bucket_prefix)
 
     # Check if objects are found
     if 'Contents' in response:
         for obj in response['Contents']:
-            # Get the key of the current object
             object_key = obj['Key']
 
-            # Skip the owner.txt file
+            # Skip the owner.txt file so it does not need to be re-created
             if object_key.endswith('owner.txt'):
                 continue
 
-            # Delete the object
             s3_client.delete_object(Bucket=bucket, Key=object_key)
 
 
